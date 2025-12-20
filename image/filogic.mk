399abd23ac817475aa913e7989b91cf9b6ad69e3
ykm0595 <163884300+ykm0595@users.noreply.github.com>
2025-12-20 23:26:25 +0800

    Update filogic.mk

--- image/filogic.mk
+++ image/filogic.mk
@@ -1,20 +1,10 @@
+
 define Device/sl3000-emmc
-  DEVICE_VENDOR := SiLuo
+  DEVICE_VENDOR := SL
   DEVICE_MODEL := SL3000
-  DEVICE_VARIANT := eMMC
+  DEVICE_VARIANT := EMMC
   DEVICE_DTS := mt7981-sl3000-emmc
-  DEVICE_DTS_DIR := ../dts
-  DEVICE_PACKAGES := \
-    kmod-usb3 \
-    kmod-usb2 \
-    kmod-usb-net \
-    kmod-usb-net-asix \
-    kmod-usb-net-rtl8152 \
-    kmod-mt7981-wmac \
-    luci \
-    block-mount \
-    kmod-fs-ext4
-  IMAGE_SIZE := 32m
-  SUPPORTED_DEVICES := sl3000-emmc
+  DEVICE_PACKAGES := kmod-mt7531 kmod-dsa kmod-dsa-mt7530 kmod-usb3 kmod-usb2 kmod-leds-gpio
+  SUPPORTED_DEVICES := sl3000
 endef
 TARGET_DEVICES += sl3000-emmc
