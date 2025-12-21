8c3ee30de9f80a3d2a93d889eaf5645732de8b7c
ykm0595 <163884300+ykm0595@users.noreply.github.com>
2025-12-21 13:19:07 +0800

    Update filogic.mk

--- image/filogic.mk
+++ image/filogic.mk
@@ -1,10 +1,12 @@
-
 define Device/sl3000-emmc
   DEVICE_VENDOR := SL
   DEVICE_MODEL := SL3000
   DEVICE_VARIANT := EMMC
   DEVICE_DTS := mt7981-sl3000-emmc
-  DEVICE_PACKAGES := kmod-mt7531 kmod-dsa kmod-dsa-mt7530 kmod-usb3 kmod-usb2 kmod-leds-gpio
+  DEVICE_PACKAGES := \
+        kmod-mt7531 kmod-dsa kmod-dsa-mt7530 \
+        kmod-mt7915e wpad-basic-mbedtls \
+        kmod-leds-gpio
   SUPPORTED_DEVICES := sl3000
 endef
 TARGET_DEVICES += sl3000-emmc
