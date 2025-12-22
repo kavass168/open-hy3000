5a53a9cb2da747ed86d3a1857219caf8e5f72fd0
ykm0595 <163884300+ykm0595@users.noreply.github.com>
2025-12-22 21:21:26 +0800

    Update filogic.mk

--- image/filogic.mk
+++ image/filogic.mk
@@ -2,13 +2,10 @@ define Device/sl3000-emmc
   DEVICE_TITLE := SL3000 (EMMC)
   DEVICE_DTS := mediatek/mt7981-sl3000-emmc
   DEVICE_PACKAGES := \
-        kmod-mt7981-firmware \
-        kmod-mt76-connac-lib \
-        kmod-mt7915e \
-        kmod-usb3 \
-        kmod-usb2 \
-        kmod-usb-ledtrig-usbport \
-        kmod-leds-gpio \
-        kmod-gpio-button-hotplug
+	kmod-mt7981-firmware \
+	kmod-mt76-connac-lib \
+	kmod-mt7915e \
+	kmod-leds-gpio \
+	kmod-gpio-button-hotplug
 endef
 TARGET_DEVICES += sl3000-emmc
